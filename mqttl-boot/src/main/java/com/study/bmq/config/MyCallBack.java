a6a362a96dca9d444489043e53f87ba668bdee2c
luohx <Lhongxiao@lcchain.com>
2021-07-19 18:58:10 +0800

    消息回退解决routingkey或queue无法接受消息问题

--- mqttl-boot/src/main/java/com/study/bmq/config/MyCallBack.java
+++ mqttl-boot/src/main/java/com/study/bmq/config/MyCallBack.java
@@ -1,6 +1,7 @@
 package com.study.bmq.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +16,7 @@ import javax.annotation.PostConstruct;
  */
 @Slf4j
 @Component
-public class MyCallBack implements RabbitTemplate.ConfirmCallback {
+public class MyCallBack implements RabbitTemplate.ConfirmCallback, RabbitTemplate.ReturnCallback {
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
@@ -26,6 +27,7 @@ public class MyCallBack implements RabbitTemplate.ConfirmCallback {
     @PostConstruct
     public void init() {
         rabbitTemplate.setConfirmCallback(this);
+        rabbitTemplate.setReturnCallback(this);
     }
 
     /**
@@ -43,5 +45,19 @@ public class MyCallBack implements RabbitTemplate.ConfirmCallback {
         } else {
             log.info("交换机还未收到 id 为:{}消息,由于原因:{}", id, cause);
         }
+    }
+
+    /**
+     * 消息传递过程中不可达目的地时将消息返回给生产者
+     *
+     * @param message
+     * @param replyCode
+     * @param replyText
+     * @param exchange
+     * @param routingKey
+     */
+    @Override
+    public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+        log.error(" 消 息 {}, 被 交 换 机 {} 退 回 ， 退 回 原 因 :{}, 路 由 key:{}", new String(message.getBody()), exchange, replyText, routingKey);
     }
 }
